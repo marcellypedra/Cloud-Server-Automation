e89d0dd76c56388f98b75a7921388ecc087f2c89
MARCELLY PEDRA <20040674@mydbs.ie>
2024-10-30 15:05:23 +0000

    up image creation for docker

--- Docker/docker_run.sh
+++ Docker/docker_run.sh
@@ -1,9 +1,33 @@
 #create a new directory for the application and install node.js
-mkdir sample-app
-cd sample-app
+mkdir express-app
+cd express-app
 sudo apt install npm
 npm init -y
 npm install express
+
+#create files required for node.js run express
+touch app.js #pull this from Github?
+touch package.json #pull this from Github?
+
+#create docker image and run docker container
+touch dockerfile #pull this from Github?
+docker build -t express-app
+docker run -p 3000:3000 express-app
+
+#check if the container is running
+docker ps -a
+docker logs [container ID]
+
+
+
+
+
+
+
+#run express from node.js
+npm start
+
+
 
 
 
