d1b56a926675032de27f8a27e8953b8145542723
MARCELLY PEDRA <20040674@mydbs.ie>
2024-11-09 19:42:28 +0000

    listen ports

--- Docker/app.js
+++ Docker/app.js
@@ -9,11 +9,11 @@ const PORT = 3000;
 
 // Define a basic route (homepage)
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Dockerizing Node Application');
 });
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', function() {
+    console.log(`The server is running on http://0.0.0.0:${PORT}`);
 });
 
