03039b6337a4ffe45114ea2ba07b09b4bdcc98a0
MARCELLY PEDRA <20040674@mydbs.ie>
2024-11-23 22:21:47 +0000

    Update ansible.sh with better paramenters for jenkins

--- ansible.sh
+++ ansible.sh
@@ -1,23 +1,23 @@
 # System Preparation
 echo "System Preparation in progress..."
-  sudo apt update
-  sudo apt install software-properties-common
+  sudo apt update -y
+  sudo apt install -y software-properties-common
   sudo apt-add-repository --yes --update ppa:ansible/ansible
-  sudo apt install ansible
+  sudo apt install -y ansible
 
 # Installing pip
 echo "Installing pip in progress..."
   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-  python3 get-pip.py --user
-  sudo apt-get install python3-pip
+  python3 get-pip.py --user --break-system-packages
+  sudo apt install -y python3-pip
     #Shows the version 
     python3 -m pip -V
 
 # Setting Up the Python Virtual Environment
 echo "Setting Up the Python Virtual Environment in progress..."
-  sudo apt install python3-venv
+  sudo apt install -y python3-venv
   python3 -m venv myenv
-  source myenv/bin/activate
+  . myenv/bin/activate
   pip install ansible
   # Creating and activating a virtual environment keeps the Ansible installation isolated from the system Python, reducing potential conflicts.
   
@@ -28,5 +28,5 @@ echo "Verifying the Ansible installation..."
 # Running Ansible to install Docker on VM
 echo "Installing Docker on VM..."
   ansible all -i inventory.ini -m ping
-  sudo usermod -aG docker MP20040674
+  sudo usermod -aG docker $(whoami)
   ansible-playbook -i inventory.ini docker_install.yml
